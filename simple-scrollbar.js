cf5fd68d97b2de168cdc4454cd8d4a97a9efdd69
Simon Warta <simon@kullo.net>
2018-02-22 20:11:43 +0100

    Add optional .ss-scrolling to define what should be scrolling

--- simple-scrollbar.js
+++ simple-scrollbar.js
@@ -51,10 +51,22 @@
 
     this.wrapper.appendChild(this.content);
 
-    while (container.firstChild) {
-      this.content.appendChild(container.firstChild);
+    var customScrollingContentContainer = container.querySelector(".ss-scrolling");
+    if (customScrollingContentContainer) {
+      // Move everything from .ss-scrolling into .ss-content
+      while (customScrollingContentContainer.firstChild) {
+        this.content.appendChild(customScrollingContentContainer.firstChild);
+      }
+      // Add wrapper into .ss-content
+      customScrollingContentContainer.appendChild(this.wrapper);
+    } else {
+      // Move everything from .ss-container into .ss-content
+      while (container.firstChild) {
+        this.content.appendChild(container.firstChild);
+      }
+      // Add wrapper into .ss-container
+      container.appendChild(this.wrapper);
     }
-    container.appendChild(this.wrapper);
 
     this.bar = d.createElement('div');
     this.bar.setAttribute('class', 'ss-scrollbar');
